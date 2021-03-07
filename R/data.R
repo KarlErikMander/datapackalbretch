102b227ffaf8a83139007b7fab621232207fb4b5
Karl Erik Mander <mander.karl@gmail.com>
2021-03-07 18:50:03 +0200

    second commit

---
+++ R/data.R
@@ -0,0 +1,30 @@
+#' Data of effort and size for training
+#'
+#' No missing values
+#' A dataset containing -----  whatever you put here.
+#'
+#' @title DATASET OF ...............
+#' @format A data frame with ....... rows and .......... variables:
+#' \describe{
+#' \item{X}{ -------.}
+#' \item{effort}{Effort measured in -------.}
+#' \item{size}{Size measured in ....}
+#' }
+#' @source \url{https://....domain.com... }
+"trainfile"
+
+
+#' Data of effort and size for training
+#'
+#' No missing values
+#' A dataset containing -----  whatever you put here.
+#'
+#' @title DATASET OF ...............
+#' @format A data frame with ....... rows and .......... variables:
+#' \describe{
+#' \item{X}{ -------.}
+#' \item{effort}{Effort measured in -------.}
+#' \item{size}{Size measured in ....}
+#' }
+#' @source \url{https://....domain.com... }
+"testfile"
